57f9c4272c1172889d22540a2b9cc960c06cade1
darco2018 <newsecond@outlook.com>
2019-03-27 20:48:16 +0100

    Declare variables in .js

---
+++ main.js
@@ -0,0 +1,23 @@
+(function () {
+
+    var totalTime = 0;
+    var trials = 8;
+    var userName = "User";
+    var instruction = "Sprawdź swoj czas reakcji. Program wyświetli ci po kolei " +
+        "8 figur geometrycznych. Twoim zadaniem jest jak najszybciej na figurę kliknąć. " +
+        "Na koniec gry zobaczysz swoje czasy reakcji i średnią wszystkich prób.";
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+    var radius = 200; // px
+    var pageMarginX = windowWidth - radius; // ?
+    var pageMarginY = windowHeight - radius; // ?
+    var userReactions = [];
+
+
+    function init() {
+        //alert("Starting...");
+    }
+
+    init();
+
+}());
