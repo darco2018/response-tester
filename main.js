c995fec6749305aef1bc9ad557f0f1c9936bfef5
darco2018 <newsecond@outlook.com>
2019-03-28 11:49:54 +0100

    Measure reaction time, end game after 8 turns

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 (function () {
 
     var totalTime = 0;
-    var trials = 8;
+    var noOfTurns = 3;
     var userName = "User";
     var instruction = "Sprawdź swoj czas reakcji. Program wyświetli ci po kolei " +
         "8 figur geometrycznych. Twoim zadaniem jest na każdą figurę jak najszybciej kliknąć. " +
@@ -11,7 +11,10 @@
     var radius = 200; // px
     var pageMarginX = windowWidth - radius; // ?
     var pageMarginY = windowHeight - radius; // ?
-    var userReactions = [];
+    var userReactionsArr = [];
+
+    var startTime;
+    var endTime;
 
     var submitBtn = document.getElementById("submitBtn");
     var userInput = document.getElementById("userName");
@@ -20,34 +23,63 @@
 
     function drawCircle() {
 
+
+
         console.log("width: " + windowWidth + ", height" + windowHeight);
 
         var xCoord = Math.floor(Math.random() * windowWidth);
         var yCoord = Math.floor(Math.random() * windowHeight);
-        console.log(xCoord + ", " + yCoord);
+        //console.log(xCoord + ", " + yCoord);
 
         var shapeClass = (Math.floor(Math.random() * 2) === 0) ? "rectangle" : "rectangle rounded";
         var shape = "<div id='shape' class='" + shapeClass + "' style='margin-left:" + xCoord + "px;" +
             "margin-top:" + yCoord + "px'></div>";
         playground.innerHTML = shape;
 
+        // start time after circle is drawn   
+        startTime = new Date().getTime();
+        console.log("shape drawn: " + startTime);
     }
 
-    function generateShape() {
+    function play() {
 
         drawCircle();
 
         document.getElementById("shape").onclick = function (e) {
+
+            // stop timer as soon as click is detected            
+            endTime = new Date().getTime();
+            console.log("Shape clicked: " + endTime);
+            measureTime(endTime, startTime);
+
             var pageX = e.pageX;
             var pageY = e.pageY;
-            alert("Got it" + pageX + ", " + pageY);
-            generateShape();
-        }
+            //alert("Got it" + pageX + ", " + pageY);
 
+
+            if(userReactionsArr.length < noOfTurns){
+                play();
+            } else {
+                endGame();
+            }            
+        }
+    }
+
+    function endGame(){
+        playground.style.display = "none";
+    }
+
+    function measureTime(end, start) {
+        var reactionTime = end - start;
+        userReactionsArr.push(reactionTime);        
+        console.log(start + ", " + end + ", " + reactionTime);
+        console.log(userReactionsArr);
+
+        
     }
 
 
-    generateShape();
+    play();
 
     /* Event listeners */
 
