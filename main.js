0d89967798a567fa6356d0514bdf8933c8e8565c
darco2018 <newsecond@outlook.com>
2019-03-28 09:45:06 +0100

    Draw rect/circle at random positions

--- main.js
+++ main.js
@@ -6,8 +6,8 @@
     var instruction = "Sprawdź swoj czas reakcji. Program wyświetli ci po kolei " +
         "8 figur geometrycznych. Twoim zadaniem jest na każdą figurę jak najszybciej kliknąć. " +
         "Na koniec gry zobaczysz swoje czasy reakcji i średnią wszystkich prób. Gdy zamkniesz to okienko, gra się ropocznie!";
-    var windowWidth = window.innerWidth;
-    var windowHeight = window.innerHeight;
+    var windowWidth = window.innerWidth; // 1600
+    var windowHeight = window.innerHeight; // 731
     var radius = 200; // px
     var pageMarginX = windowWidth - radius; // ?
     var pageMarginY = windowHeight - radius; // ?
@@ -19,15 +19,32 @@
     var playground = document.getElementById("playground");
 
     function init() {
-        //alert("Starting...");
+
+        playground.innerHTML = generateShape();
+    }
+
+    function generateShape() {
+
+            console.log("width: " + windowWidth + ", height" + windowHeight);
+
+        var xCoord = Math.floor(Math.random() * windowWidth);
+        var yCoord = Math.floor(Math.random() * windowHeight);
+            console.log(xCoord + ", " + yCoord);
+            
+        var shapeClass = (Math.floor(Math.random() * 2) === 0) ? "rectangle" : "rectangle rounded";
+        var shape = "<div class='" + shapeClass + "' style='margin-left:" + xCoord + "px;" +
+            "margin-top:" + yCoord + "px'></div>";
+        return shape;
+
     }
 
     /* Event listeners */
 
-    submitBtn.onclick = function(e){
+    submitBtn.onclick = function (e) {
         introDiv.style.display = "none";
-        userName = userInput.value;        
+        userName = userInput.value;
         alert("Hi " + userName + "! " + instruction);
+        startGame();
     }
 
     init();
